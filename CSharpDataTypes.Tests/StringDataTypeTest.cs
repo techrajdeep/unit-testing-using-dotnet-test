304f9bda96f986b3a21899d7c9abbd6387a1d781
techrajdeep <rajdeepsahoo2012@gmail.com>
2024-06-20 20:56:21 +0530

    add string test cases

--- CSharpDataTypes.Tests/StringDataTypeTest.cs
+++ CSharpDataTypes.Tests/StringDataTypeTest.cs
@@ -9,5 +9,22 @@ namespace CSharpDataTypesTests{
             var result= data.fullName("Rajdeep","Sahoo");
             Assert.Equal("Rajdeep Sahoo",result);
         }
+
+        [Fact]
+        public void Test_Null_nickname(){
+            var data = new StringDataTypes();
+            Assert.Null(data.nickName);
+        }
+
+        [Fact]
+        public void Test_FullName_Non_Empty()
+        {
+            // Given
+            var data = new StringDataTypes();
+            // When
+            var result=data.fullName("Abc","Def");
+            // Then
+            Assert.False(string.IsNullOrEmpty(result));
+        }
     }
 }
